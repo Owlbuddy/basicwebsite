42ec4c4e57952c98a2115159534e3b6ec393cd9f
Johnny Prime <theLephty@gmail.com>
2015-11-24 09:29:00 -0700

    increase legibility

--- js/leaves_falling.js
+++ js/leaves_falling.js
@@ -11,8 +11,8 @@ var rotation = 1; //define rotation of leaves
 var rotationTrue = 1 //whether leaves rotate (1) or not (0)
 var numberOfLeaves = 30; //define number of leaves
 var size = 120; //general size of leaves, final size is calculated randomly (with this number as general parameter)
-var typeOfLeaf = 0; //type of leav, 0 - maple leaf, 1 - saw leaf, 2 - normal leaf, 3 all types together
-var fps = 60;
+var typeOfLeaf = 3; //type of leav, 0 - maple leaf, 1 - saw leaf, 2 - normal leaf, 3 all types together
+var fps = 30;
 
 Ypos = new Array();
 Xpos = new Array();
